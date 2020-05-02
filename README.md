0995a305141163fabf83b9790cef415a7ac7849f
Dunkmania101 <50186904+Dunkmania101@users.noreply.github.com>
2020-05-01 23:03:52 -0400

    Fix initial upload

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # Python Music Generator
 
 A Python script that randomly generates "music"
+
+This is achieved by the following 5 step plan:
+1. Pick a random starting note and add it to a list.
+2. Add two other random notes for step 4.
+3. Loop through step 4 for the number of notes needed in the piece.
+4. Multiply one of the last few generated notes by a random ammount such that
+the result is a multiple of 2 or 3 steps away from the previous and add
+the result to the list.
+5. Do some output with the list.
+
+
+It plays through Fluidsynth with an sf2 pack.
+Feel free to change the output system if you need / want something else.
+
+__Installing Dependencies ==>__
+
+For ubuntu:
+$ sudo apt install python3 python3-pip fluidsynth 
+
+For other distributions:
+Use your package manager to install __python3 python3-pip fluidsynth__
+
+Finally:
+$ pip3 install mingus
--- music.py
+++ music.py
@@ -1,80 +1,84 @@
-# ----------
-# Set to "True" for piano sound
-# Set to "False" for sine-wave sound
-piano = True
-
-# Set to the desired time between / length of notes
-seconds = 0.3
-# ----------
-
-
 from random import randint as rint
-if piano:
-    from mingus.midi import fluidsynth
-    fluidsynth.init('/usr/share/sounds/sf2/FluidR3_GM.sf2', "alsa")
-    import time
-else:
-    import numpy as np
-    import simpleaudio as sa
+from time import sleep
+from mingus.midi import fluidsynth
+
+# ----------
+# Path to sf2 file (Do not output sound if empty string):
+sf2 = ""
+# Default pack installed with fluidsynth:
+# sf2 = "/usr/share/sounds/sf2/FluidR3_GM.sf2"
+
+# Tell what sound driver to use for the sf2 sound:
+# ("alsa" may be needed for linux users)
+sound_driver = "alsa"
+
+# Set to the desired time between / length of notes:
+seconds = 0.3
+
+# Set to the desired number of notes:
+stop_point = 5000
+# ----------
 
 
-keys = {
-    1: "c",
-    2: "c#",
-    3: "d",
-    4: "d#",
-    5: "e",
-    6: "f",
-    7: "f#",
-    8: "g",
-    9: "g#",
-    10: "a",
-    11: "a#",
-    12: "b"
-}
+if sf2 != "" and sound_driver != "":
+    fluidsynth.init(sf2, sound_driver)
+elif sf2 != "" and sound_driver == "":
+    fluidsynth.init(sf2)
+elif sf2 == "" and sound_driver != "":
+    fluidsynth.init(None, sound_driver)
+elif sf2 == "" and sound_driver == "":
+    fluidsynth.init(None)
 
+
+# ----------
+# Main mechanism:
 notes = [rint(1, 12)]
-notes.append((notes[-1]*rint(2, 3)))
-notes.append(int(notes[-1]/rint(2, 3)))
-for l1 in range(0, 1000):
+notes.append((notes[-1]*rint(4, 6)))
+notes.append(int(notes[-1]/rint(4, 6)))
+for l1 in range(0, stop_point):
     if notes[-1] < 2:
         notes[-1] += 1
     if notes[-1] > 6:
-        notes.append(int(notes[-rint(1, 3)]/rint(2, 3)))
+        notes.append(int(notes[-rint(1, 3)]/rint(4, 6)))
     elif notes[-1] < 5:
-        notes.append(int(notes[-rint(1, 3)]*rint(2, 3)))
+        notes.append(int(notes[-rint(1, 3)]*rint(4, 6)))
     else:
         if rint(1, 2) == 1:
-            notes.append(int(notes[-rint(1, 3)]*rint(2, 3)))
+            notes.append(int(notes[-rint(1, 3)]*rint(4, 6)))
         else:
-            notes.append(int(notes[-rint(1, 3)]/rint(2, 3)))
+            notes.append(int(notes[-rint(1, 3)]/rint(4, 6)))
 print(notes)
-
-lout = []
-for out in notes:
-    if out in keys:
-        lout.append(keys[out])
-    else:
-        octave = int(out/12)
-        lout.append(f"{keys[out+1-(octave*12)]} oct. {octave}")
-print(lout)
+# ----------
 
 
-if piano:
+# This part is supposed to print out the actual musical notes
+# instead of the ugly numbers.
+# Broken for now...
+# keys = {
+#     1: "c",
+#     2: "c#",
+#     3: "d",
+#     4: "d#",
+#     8: "g",
+#     9: "g#",
+#     10: "a",
+#     11: "a#",
+#     12: "b"
+# }
+#
+# lout = []
+# for out in notes:
+#     if out in keys:
+#         lout.append(keys[out])
+#     else:
+#         octave = int(out/12)
+#         lout.append(f"{keys[out+1-(octave*12)]} oct. {octave}")
+# print(lout)
+
+if sf2 != "":
     for play in notes:
-        if play < 50:
+        if play < 15:
             fluidsynth.play_Note(play*5, 0, 100)
         else:
             fluidsynth.play_Note(play*2, 0, 100)
-        time.sleep(seconds)
-else:
-    a = 2**(1/12)
-    fs = 44100
-    for play in notes:
-        frequency = 261.63*(a**play)
-        t = np.linspace(0, seconds, seconds * fs, False)
-        note = np.sin(frequency * t * 2 * np.pi)
-        audio = note * (2**15 - 1) / np.max(np.abs(note))
-        audio = audio.astype(np.int16)
-        play_obj = sa.play_buffer(audio, 1, 2, fs)
-        play_obj.wait_done()
+        sleep(seconds)
